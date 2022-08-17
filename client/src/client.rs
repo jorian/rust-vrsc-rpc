8005d728bf491a98ca2e4421485ec58a72a6557c
jorian <10341677+jorian@users.noreply.github.com>
2022-08-17 14:20:48 +0200

    reference

--- client/src/client.rs
+++ client/src/client.rs
@@ -321,7 +321,7 @@ pub trait RpcApi: Sized {
     fn registeridentity(
         &self,
         namecommitment: &NameCommitment,
-        addresses: Vec<Address>,
+        addresses: Vec<&Address>,
         minimum_signatures: Option<u8>,
         private_address: Option<String>,
         currency_name: Option<String>,
@@ -337,7 +337,7 @@ pub trait RpcApi: Sized {
         #[derive(Serialize)]
         struct Identity<'a> {
             name: &'a str,
-            primaryaddresses: Vec<Address>,
+            primaryaddresses: Vec<&'a Address>,
             #[serde(skip_serializing_if = "Option::is_none")]
             minimumsignatures: Option<u8>,
             #[serde(skip_serializing_if = "Option::is_none")]
